2f36435c9c675fc3ab77bb823b11bd9dc5a1d109
Atom <sargis.shokhikyan@tumo.org>
2023-10-27 15:27:00 +0400

    MyGame project

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 app.get("/", function (req, res) {
-    res.redirect("index.html");
+    res.redirect("/index1.html");
 });
 
 server.listen(3000, function () {
